02f52c82eba01030fd58de7e612b697ee55e47bc
Saksham Tyagi <saksham@Sakshams-Mac-mini.local>
2025-06-12 23:29:52 +0530

    updated backend

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+dotenv.config();
+
 import connectDB from './config/db.js';
 import dataRoutes from './routes/dataRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 
 
-dotenv.config();
 
 const app = express();
 
